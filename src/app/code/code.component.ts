b224d53fe7b8348231db2cd59767f33c1348e649
Peter Iskandar <bassingitup@gmail.com>
2020-07-01 16:10:46 -0400

    added paging for blog and page control

--- src/app/code/code.component.ts
+++ src/app/code/code.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {BlogPage, BlogPost} from '../model/blog-post';
 import {PiskandarService} from '../api/piskandar.service';
-import * as moment from 'moment'; // add this 1 of 4
+import * as moment from 'moment';
+import {ActivatedRoute, Router} from '@angular/router'; // add this 1 of 4
 
 @Component({
   selector: 'app-code',
@@ -12,18 +13,34 @@ export class CodeComponent implements OnInit {
 
   page: BlogPage;
   private momentInstance = moment();
-  currentPageNumber = 1;
+
   pageSize = 10;
   totalPages = 10;
+  loading = false;
+  pageNumber = 0;
+  totalItems = 0;
 
-  constructor(private piskandarService: PiskandarService) {
+  constructor(private piskandarService: PiskandarService,
+              private route: ActivatedRoute,
+              private router: Router) {
 
   }
 
   ngOnInit() {
-    this.piskandarService.getPosts('Publish', this.currentPageNumber, this.pageSize).subscribe(posts => {
-      this.page = posts;
-      this.totalPages = this.page.totalPages;
+    this.loading = true;
+    this.route.queryParams.subscribe(params => {
+      if (params.p) {
+
+        this.pageNumber = +params.p;
+      } else {
+        this.pageNumber = 0;
+      }
+      this.piskandarService.getPosts('Publish', this.pageNumber, this.pageSize).subscribe(posts => {
+        this.page = posts;
+        this.totalPages = this.page.totalPages;
+        this.totalItems = this.page.totalItems;
+        this.loading = false;
+      });
     });
   }
 
@@ -37,5 +54,43 @@ export class CodeComponent implements OnInit {
 
   formatDay(d: Date) {
     return moment(d).format('DD');
+  }
+
+  goToNewerPage() {
+    this.pageNumber--;
+    if (this.pageNumber < 0) {
+      this.pageNumber = 0;
+    }
+    this.router.navigate(['/code'], {queryParams: {p: this.pageNumber}});
+  }
+
+
+  goToOlderPage() {
+    this.pageNumber++;
+    if (this.pageNumber > this.totalPages - 1) {
+      this.pageNumber = this.totalPages - 1;
+    }
+    this.router.navigate(['/code'], {queryParams: {p: this.pageNumber}});
+
+  }
+
+  goToPage(pageNumber: number) {
+    if (pageNumber < 0) {
+      return;
+    }
+    if (pageNumber >= this.totalPages) {
+      return;
+    }
+
+    this.pageNumber = pageNumber;
+    this.router.navigate(['/code'], {queryParams: {p: this.pageNumber}});
+  }
+
+  isNewerEnabled() {
+    return this.pageNumber !== 0;
+  }
+
+  isOlderEnabled() {
+    return this.pageNumber + 1 !== this.totalPages;
   }
 }
--- src/app/page-control/page-control.component.ts
+++ src/app/page-control/page-control.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-page-control',
@@ -6,6 +6,13 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./page-control.component.scss']
 })
 export class PageControlComponent implements OnInit {
+
+  @Input() newerEnabled = true;
+  @Input() olderEnabled = true;
+
+  @Output() goToNewer = new EventEmitter();
+  @Output() goToOlder = new EventEmitter();
+  @Output() goToPage = new EventEmitter<number>();
 
   @Input()
   totalPages: number;
@@ -19,7 +26,21 @@ export class PageControlComponent implements OnInit {
   }
 
   counter(i: number) {
-    return new Array(i);
+    const array = new Array();
+    for (let j = 1; j < i + 1; j++) {
+      array.push(j);
+    }
+    return array;
+  }
+  newer() {
+    this.goToNewer.emit();
   }
 
+  older() {
+    this.goToOlder.emit();
+  }
+
+  page(pageNumber) {
+    this.goToPage.emit(pageNumber - 1);
+  }
 }
